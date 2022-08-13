eba560a24afded25fabf078bee1bdcfe586b85dc
ArizCefDev <106917027+ArizCefDev@users.noreply.github.com>
2022-08-13 19:07:48 +0400

    .Net
    .NET

---
+++ WebSignalR/WebSignalR/Controllers/MessageController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSignalR.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
